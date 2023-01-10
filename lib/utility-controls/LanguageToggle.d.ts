30e6ce42f062e78e09392f2714241aa17ccdc0ce
NetsydeMiro <miro@netsyde.com>
2023-01-10 18:05:27 -0500

    language toggle typo

--- lib/utility-controls/LanguageToggle.d.ts
+++ lib/utility-controls/LanguageToggle.d.ts
@@ -2,6 +2,6 @@
 export interface LanguageToggleProps<LanguageT extends string> {
     currentLanguage: LanguageT;
     languages: Array<LanguageT>;
-    onSelectLanguage: (language: LanguageT) => void;
+    onLanguageChange: (language: LanguageT) => void;
 }
 export declare function LanguageToggle<LanguageT extends string>(props: LanguageToggleProps<LanguageT>): JSX.Element;
--- src/utility-controls/LanguageToggle.tsx
+++ src/utility-controls/LanguageToggle.tsx
@@ -3,16 +3,16 @@ import Button from '../inputs/Button'
 export interface LanguageToggleProps<LanguageT extends string> {
 	currentLanguage: LanguageT
 	languages: Array<LanguageT>
-	onSelectLanguage: (language: LanguageT) => void
+	onLanguageChange: (language: LanguageT) => void
 }
 
 export function LanguageToggle<LanguageT extends string>(props: LanguageToggleProps<LanguageT>) {
-	const { currentLanguage, languages, onSelectLanguage } = props
+	const { currentLanguage, languages, onLanguageChange } = props
 
 	const languageIndex = languages.indexOf(currentLanguage)
 	let nextLanguageIndex = languageIndex + 1
 	if (nextLanguageIndex >= languages.length) nextLanguageIndex = 0
 	const nextLanguage = languages[nextLanguageIndex]
 
-	return <Button text={`Toggle Language ${currentLanguage} -> ${nextLanguage}`} onClick={() => onSelectLanguage(nextLanguage)} />
+	return <Button text={`Toggle Language ${currentLanguage} -> ${nextLanguage}`} onClick={() => onLanguageChange(nextLanguage)} />
 }
