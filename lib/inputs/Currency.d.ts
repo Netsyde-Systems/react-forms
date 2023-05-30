18a14505d76195fc53d57cc923ab0fc102de504a
NetsydeMiro <miro@netsyde.com>
2023-05-29 22:14:13 -0400

    compiled and bumped version

--- lib/inputs/Currency.d.ts
+++ lib/inputs/Currency.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="react" />
 import { InputProps } from './inputs';
-import './Inputs.scss';
 export declare function Currency(props: InputProps<number>): JSX.Element;
 export default Currency;
--- lib/inputs/MaskedInput.d.ts
+++ lib/inputs/MaskedInput.d.ts
@@ -5,8 +5,16 @@ import './Inputs.scss';
 export interface MaskTestFunction {
     (val: string): boolean;
 }
+export interface MaskedInputDetailedChangedEvent {
+    value?: string;
+    typedValue?: string;
+}
+export interface MaskedInputDetailedChangeEventHandler {
+    (e: MaskedInputDetailedChangedEvent): void;
+}
 export interface MaskedInputProps extends InputProps<string> {
     mask: string | AnyMaskedOptions;
+    onChangeDetailed?: MaskedInputDetailedChangeEventHandler;
 }
 export declare function MaskedInput(props: MaskedInputProps): JSX.Element;
 export default MaskedInput;
